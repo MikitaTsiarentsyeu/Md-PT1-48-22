dc492649e2689542c29cea1901c00411769dffa4
Mikita Tsiarentsyeu <Mikita_Tsiarentsyeu@epam.com>
2022-04-30 15:01:16 +0600

    add lesson 30.04

---
+++ Lessons/30.04/num_desc.py
@@ -0,0 +1,61 @@
+import decimal
+import fractions
+import math
+import random
+
+5
+78
+-45
+
+x = 45
+print(type(x))
+
+print(12*3-2)
+
+print(type(12/4))
+print(type(12//4))
+
+print(15//4)
+
+print(int(2.8))
+print(int(-2.8))
+
+print(type(int('142')))
+
+print(round(4.5))
+
+print(type(5.6))
+
+print(4+4.0)
+print(type(float('-3.5')))
+print(float(4))
+
+
+
+x = decimal.Decimal('1.1')
+print(x+x+x)
+print(x)
+
+x = decimal.Decimal(1.1)
+print(x+x+x)
+print(x)
+
+print(fractions.Fraction(10))
+print(fractions.Fraction('2.8'))
+print(fractions.Fraction('3.3')*fractions.Fraction('2.8'))
+print(fractions.Fraction('1.1'))
+
+print(math.cos(math.pi))
+print(math.sqrt(144))
+print(144**0.5)
+print(math.factorial(10))
+
+print(random.randint(1,10))
+print(random.randint(1,10))
+print(random.randint(1,10))
+print(random.randint(1,10))
+print(random.randint(1,10))
+print(random.randint(1,10))
+print(random.randint(1,10))
+print(random.randint(1,10))
+print(random.randint(1,10))
