ba0547ee5c70bc16ac563291a9ebc301cdb51559
SEO-25 <100795115+SEO-25@users.noreply.github.com>
2022-05-15 12:35:19 +0300

    use_decimal

--- Tasks/Semashko/Task1/deposit_calc.py
+++ Tasks/Semashko/Task1/deposit_calc.py
@@ -1,8 +1,10 @@
-# How can we use Decimal to get the tolerance we want???
-P = float(input('Enter your deposit (BYN):\n'))
-N = float(input('Enter annual percent (%):\n'))
+import decimal
+
+P = decimal.Decimal(input('Enter your deposit (BYN):\n'))
+N = decimal.Decimal(input('Enter annual percent (%):\n'))
 Y = int(input('How many years?\n'))
 
 S = P * (1 + N / 100 / 12) ** (Y * 12)
-# format S 3 signs after "."
+
+# format S 2 signs after "."
 print ('Total sum: {:.2f} BYN'.format(S))
