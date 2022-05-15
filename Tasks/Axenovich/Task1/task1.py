8de2594f4b887bd11d285a9f82814f513ad00997
Nikolay Aksenovich <nikolay.aksenovich@gmail.com>
2022-05-15 11:58:43 +0300

    Update task1.py

--- Tasks/Axenovich/Task1/task1.py
+++ Tasks/Axenovich/Task1/task1.py
@@ -1,9 +1,10 @@
+from decimal import *
 print('Calculating a deposit with monthly capitalisation\n')
 
-deposit = int(input('Input your deposit amount(BYN): '))
-percent = int(input('Input annual deposit interest(%): '))
-years = int(input('Input term of the deposit(years): '))
+deposit = Decimal(input('Input your deposit amount(BYN): '))
+percent = Decimal(input('Input annual deposit interest(%): '))
+years = Decimal(input('Input term of the deposit(years): '))
 
-result = deposit * ((1 + percent/100/12)**(years*12))
+result = round(deposit * ((1 + percent/100/12)**(years*12)), 2)
 
-print(f'Your {deposit} BYN after {years}', 'years' if years > 1 else 'year', f'with {percent}% per year will be {int(result)} BYN')
+print(f"Your {deposit} BYN after {years} {'years' if years > 1 else 'year'} with {percent}% per year will be {result} BYN")
