9d43ebc67342422758f2a1f314d93f724cbdfdb5
Vadzim Kashko <vadim.kashko@gmail.com>
2022-05-17 16:39:44 +0300

    Fixes and tests

--- Tasks/Kashko/task_02/task_02.py
+++ Tasks/Kashko/task_02/task_02.py
@@ -1,4 +1,4 @@
-import datetime as dt
+from datetime import datetime
 
 HOURS = [['один', 'первого', 'час'], ['два', 'второго', 'часа'],
          ['три', 'третьего', 'часа'], ['четыре', 'четвертого', 'часа'],
@@ -6,7 +6,7 @@ HOURS = [['один', 'первого', 'час'], ['два', 'второго', 
          ['семь', 'седьмого', 'часов'], ['восемь', 'восьмого', 'часов'],
          ['девять', 'девятого', 'часов'], ['десять', 'десятого', 'часов'],
          ['одиннадцать', 'одиннадцатого', 'часов'],
-         ['двеннадцать', 'двеннадцатого', 'часов']]
+         ['двенадцать', 'двенадцатого', 'часов']]
 
 MINUTES = {
     1: ['одна', 'одной', 'минута', 'минуты'],
@@ -20,7 +20,7 @@ MINUTES = {
     9: ['девять', 'девяти', 'минут', 'минут'],
     10: ['десять', 'десяти', 'минут', 'минут'],
     11: ['одиннадцать', 'одиннадцати', 'минут', 'минут'],
-    12: ['двеннадцать', 'двеннадцати', 'минут', 'минут'],
+    12: ['двенадцать', 'двенадцати', 'минут', 'минут'],
     13: ['тринадцать', 'тринадцати', 'минут', 'минут'],
     14: ['четырнадцать', 'четырнадцати', 'минут', 'минут'],
     15: ['пятнадцать', 'пятнадцати', 'минут', 'минут'],
@@ -29,38 +29,47 @@ MINUTES = {
     18: ['восемнадцать', 'восемнадцати', 'минут', 'минут'],
     19: ['девятнадцать', 'девятнадцати', 'минут', 'минут'],
     20: ['двадцать', 'двадцати', 'минут', 'минут'],
-    30: ['тридцать'],
-    40: ['сорок']
+    30: ['тридцать', 'тридцати', 'минут', 'минут'],
+    40: ['сорок', 'сорока', 'минут', 'минут'],
 }
 
 
-def input_time() -> dt.datetime:
-    """
-    Returns time as dt.time object.
+def input_time() -> datetime:
+    """Reads user input and returns time as datetime object.
+
     Input time in format: HH:MM or empty string to get current time.
-    :return: dt.time object
+
+    Returns:
+        datetime: datetime object
     """
     while True:
+
         try:
             user_time = input('Enter time as HH:MM\n')
             if user_time == '':
-                user_time = dt.datetime.now()
+                user_time = datetime.now()
                 break
-            user_time = dt.datetime.strptime(user_time, '%H:%M')
+            user_time = datetime.strptime(user_time, '%H:%M')
             break
 
         except ValueError as error:
             print(f'Invalid time format: {error}')
+
     return user_time
 
 
-def time_to_text(time: dt.datetime) -> str:
-    """
-    Returns time in text format.
-    :param time: time in time format
-    :return: time in text format
+def time_to_text(time: datetime) -> str:
+    """Represents datetime object as words in Russian
+    and returns time in text format.
+
+    Args:
+        time (datetime): datetime object to convert
+
+    Returns:
+        str: string with time as words
     """
     text_time = time.strftime("%H:%M")
+
     if time.hour >= 12:
         h = time.hour - 12
     else:
@@ -79,8 +88,13 @@ def time_to_text(time: dt.datetime) -> str:
         text = f"{text_time} - без {MINUTES[60-m][1]} " \
                f"{MINUTES[60-m][3]} {HOURS[h][0]}"
     else:
-        text = f"{text_time} - {MINUTES[(m // 10)*10][0]} " \
+        if not m % 10:
+            text = f"{text_time} - {MINUTES[m][0]} " \
+                   f"{MINUTES[m][2]} {HOURS[h][1]}"
+        else:
+            text = f"{text_time} - {MINUTES[(m // 10)*10][0]} " \
                f"{MINUTES[m % 10][0]} {MINUTES[m % 10][2]} {HOURS[h][1]}"
+
     return text
 
 
