ae69f0a7b37fdb90bde5f8085e44f80ea0a4e93d
Rc561U <hugeanton29011994@gmail.com>
2022-06-04 11:55:57 +0300

    update and add new

--- Tasks/Ramancou/Task4/Task4_remastered.py
+++ Tasks/Ramancou/Task4/Task4_remastered.py
@@ -1,6 +1,8 @@
+from math import prod
+from itertools import pairwise, chain
 variable = ['five thirteen two eleven seventeen two one thirteen ten four eight five nineteen',
             {'seventeen': 17, 'eight': 8, 'two': 2, 'one': 1, 'four': 4, 'thirteen': 13, 'five': 5, 'eleven': 11, 'nineteen': 19, 'ten': 10}]
 for _ in variable[0].split(): variable[0] = variable[0].replace(_, str(variable[1][_]))
-variable = list(set(map(int, variable[0].split())))
-print(*list(map(lambda x: variable[x] * variable[x + 1] if x % 2 == 0 else variable[x] + variable[x + 1], range(9)))) # Calculate products of 1 and 2elements, sums of 2 and 3, etc.
-print(sum([i for i in variable if i % 2 != 0]))  # Calculate sum of odd numbers
+variable = list(pairwise(set(map(int, variable[0].split()))))
+print(*list(map(lambda x: prod(x) if variable.index(x) % 2 == 0 else sum(x), variable)))
+print(sum(filter(lambda x: x % 2 != 0, list(set(chain.from_iterable(variable))))))
