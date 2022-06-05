1d49e265dad966fb8b4c93d339d339b9d46023ea
AlexandrPetrechkiv <104298490+AlexandrPetrechkiv@users.noreply.github.com>
2022-06-05 09:17:53 +0300

    Update Task 4

--- Tasks/Petrechkiv/Task4/task4.py
+++ Tasks/Petrechkiv/Task4/task4.py
@@ -2,8 +2,10 @@ numbers = ['five thirteen two eleven seventeen two one thirteen ten four eight f
            {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'ten': 10, 'eleven': 11,
             'twelve': 12, 'thirteen': 13, 'fourteen': 14, 'fifteen': 15, 'sixteen': 16, 'seventeen': 17, 'eighteen': 18, 'nineteen': 19,
              'twenty': 20}]
-numbers = list({numbers[1][n] for n in numbers[0].split()})
-print(f'numbers: {numbers}\n'
+numbers = [numbers[1][n] for n in numbers[0].split()]
+print ('numbers:',numbers)             
+numbers = list(set(numbers))
+print(f'numbers sort: {numbers}\n'
       f'The product of numbers: {[numbers[::2][n] * numbers[1::2][n] for n in range(min(len(numbers[::2]), len(numbers[1::2])))]}\n'
       f'Sum of numbers: {[numbers[1::2][n] + numbers[2::2][n] for n in range(min(len(numbers[1::2]), len(numbers[2::2])))]}\n'
       f'The sum of odd numbers: {sum([n for n in numbers if n % 2 != 0])}')
