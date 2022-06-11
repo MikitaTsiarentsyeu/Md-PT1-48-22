80ae7515dbf58db69836b0213e07906344cb73c8
OlgaDecher <oka-666666@bk.ru>
2022-06-11 11:51:03 +0300

    Update task4.py

--- Tasks/Chernova_Olga/Task4/task4.py
+++ Tasks/Chernova_Olga/Task4/task4.py
@@ -1,5 +1,5 @@
 def check_str(text: str) -> str: 
-    """This function counts the number of lowercase 
+    """The function counts the number of lowercase 
     and uppercase letters in a string\n
     text - any string"""
     text = ''.join(text.split())
@@ -7,33 +7,33 @@ def check_str(text: str) -> str:
     for i in text:
         if i.islower():
             counter += 1
-    print(f'There are {len(text) - counter} upper case and {counter} lower case')
+    return f'There are {len(text) - counter} upper case and {counter} lower case'
 
-check_str('ThiS is TEST texT for ThE tasK')
+print(check_str('ThiS is TEST texT for ThE tasK'))
 
 
 
 def is_prime(num: int) -> bool:
-    """This function determines whether the number is prime or not\n
+    """The function determines whether the number is prime or not\n
     num - positive integer greater than 0""" 
     if num == 2:
-        print('True')
+        return True
     elif num == 1 or num % 2 == 0:
-        print('False')
+        return False
     else:
         work_num = 3
         while work_num ** 2 <= num:
             if num % work_num == 0:
-                print('False')
+                return False
             work_num += 2
-        print('True')
+        return True
                     
-is_prime(149)
+print(is_prime(787))
 
 
 
 def get_ranges(l:list) -> str: 
-    """This function collapses the list. If the numbers are in order, then 
+    """The function collapses the list. If the numbers are in order, then 
     the first and final numbers are hyphenated. If the number is one, then this number 
     is output.
     l - a list with non-repeating integers sorted in ascending order"""
@@ -58,7 +58,7 @@ def get_ranges(l:list) -> str:
         else:
             list_of_output.append(work_list[0])
             break
-    print(', '.join(list_of_output))
+    return ', '.join(list_of_output)
         
             
-get_ranges([-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11,14,15,17,18,19,20])
+print(get_ranges([-6,-3,-2,-1,0,1,3,4,5,7,8,9,10,11,14,15,17,18,19,20]))
