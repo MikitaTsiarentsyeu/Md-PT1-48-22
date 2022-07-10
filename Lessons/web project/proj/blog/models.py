7da1b41862bebcc2292b9f7c3be4b105a97e6f18
Mikita Tsiarentsyeu <mikita_tsiarentsyeu@epam.com>
2022-07-10 14:54:56 +0600

    add lesson 10/07

---
+++ Lessons/web project/proj/blog/models.py
@@ -0,0 +1,22 @@
+import email
+from pyexpat import model
+from django.db import models
+
+class Author(models.Model):
+
+    name = models.CharField(blank=False, max_length=100)
+    email = models.EmailField(blank=False, primary_key=True)
+
+class Post(models.Model):
+
+    POST_TYPES = [('c', 'copyright'), ('a', 'author')]
+
+    title = models.CharField(blank=False, max_length=200)
+    subtitle = models.CharField(max_length=500)
+    content = models.TextField(blank=False)
+    issued = models.DateTimeField()
+    post_type = models.CharField(max_length=1, blank=False, choices=POST_TYPES)
+    image = models.ImageField(upload_to='uploads')
+
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+
