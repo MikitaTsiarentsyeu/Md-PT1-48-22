60821e72bf18d2edc33f906ca4d743f0b3dda81e
mpilkou <mpilkou@gmail.com>
2022-07-25 10:54:14 +0300

    add frontend

--- Tasks/Pilkov/FinalProject/project/courses/models.py
+++ Tasks/Pilkov/FinalProject/project/courses/models.py
@@ -6,12 +6,11 @@ from django.core.validators import FileExtensionValidator
 class Course(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=200, blank=False)
-
+    img = models.ImageField(upload_to='img')
 
 class Lection(models.Model):
     title = models.CharField(max_length=80, blank=False)
-    subtitle = models.CharField(max_length=100, blank=False)
     text = models.TextField(blank=True)
-    video = models.FileField(upload_to='uploads', validators=[FileExtensionValidator(allowed_extensions=['mp4','webm'])])
+    video = models.FileField(upload_to='videos', validators=[FileExtensionValidator(allowed_extensions=['mp4','webm'])])
 
     course = models.ForeignKey(Course, on_delete = models.CASCADE)
--- Tasks/Pilkov/FinalProject/project/project/urls.py
+++ Tasks/Pilkov/FinalProject/project/project/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
+    path('', lambda req: render(req, 'home.html'), name='admin'),
 ]
