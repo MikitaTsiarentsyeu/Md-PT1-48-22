677dba7a00169771e02700efa6f0d24f580cc4fd
mpilkou <mpilkou@gmail.com>
2022-07-25 14:04:33 +0300

    Update models.py

--- Tasks/Pilkov/FinalProject/project/courses/models.py
+++ Tasks/Pilkov/FinalProject/project/courses/models.py
@@ -4,9 +4,12 @@ from django.core.validators import FileExtensionValidator
 # Create your models here.
 
 class Course(models.Model):
-    name = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.CharField(max_length=200, blank=False)
-    img = models.ImageField(upload_to='img')
+    image = models.ImageField(upload_to='img')
+
+    def __str__(self):
+        return self.name
 
 class Lection(models.Model):
     title = models.CharField(max_length=80, blank=False)
@@ -14,3 +17,6 @@ class Lection(models.Model):
     video = models.FileField(upload_to='videos', validators=[FileExtensionValidator(allowed_extensions=['mp4','webm'])])
 
     course = models.ForeignKey(Course, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.title
