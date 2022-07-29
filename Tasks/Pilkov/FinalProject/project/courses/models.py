4270b7601886186326ded42a1487dc323829333b
mpilkou <mpilkou@gmail.com>
2022-07-29 22:56:03 +0300

    change courses representation and form

--- Tasks/Pilkov/FinalProject/project/courses/models.py
+++ Tasks/Pilkov/FinalProject/project/courses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.core.validators import FileExtensionValidator
 
 # Create your models here.
@@ -7,6 +8,8 @@ class Course(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.CharField(max_length=200, blank=False)
     image = models.ImageField(upload_to='img')
+
+    user = models.ForeignKey(User, blank=False, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- Tasks/Pilkov/FinalProject/project/courses/views.py
+++ Tasks/Pilkov/FinalProject/project/courses/views.py
@@ -1,5 +1,7 @@
+from itertools import count
 from django.shortcuts import redirect, render
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from courses.models import Course, Lection
 from courses.forms import AddCourseModelForm
@@ -11,14 +13,17 @@ class CoursesView(View):
         courses = Course.objects.all()
         return render(req, 'home.html', { 'courses': courses })
 
-class AddCourseView(View):
+class AddCourseView(LoginRequiredMixin, View):
     def get(self, req):
         return render(req, 'add_course.html', {'form': AddCourseModelForm()})
 
     def post(self, req):
         form = AddCourseModelForm(req.POST, req.FILES)
         if form.is_valid():
-            form.save()
+            course = form.save(commit=False)
+            course.user = req.user
+            course.save()
+            form.save_m2m()
             return redirect('courses')
         return render(req, 'add_course.html', {'form': AddCourseModelForm()})
 
@@ -26,5 +31,6 @@ class AddCourseView(View):
 class LectionsView(View):
 
     def get(self, req, course_id):
+        course = Course.objects.get(id = course_id)
         lections = Lection.objects.filter(course_id = course_id)
-        return render(req, 'lections.html', { 'lections': lections })
+        return render(req, 'lections.html', { 'course': course, 'lections': lections })
