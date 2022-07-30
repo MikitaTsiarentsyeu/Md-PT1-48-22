3a55aadcfd5c0865f8fac3c21241d5cb1cb2d811
mpilkou <mpilkou@gmail.com>
2022-07-30 09:48:32 +0300

    secret key to file

--- Tasks/Pilkov/FinalProject/project/project/settings.py
+++ Tasks/Pilkov/FinalProject/project/project/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_o=sk=pg7is&#cqksic)4ovlir1%rx=uex25&bd^vcx!n1-&03'
+with open('secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
